ee59b5185942a8fa1f7f617f659a5fe9f4f12ad1
Rayssa <rayssa.cardoso.gonc@gmail.com>
2024-03-14 11:58:29 -0300

    Primeiro commit

---
+++ Frog/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Frog;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
